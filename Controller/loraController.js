ccd137afce6d5002a221498204c3974d12bbb1ba
Guhan-K <guhan5936@gmail.com>
2024-04-27 10:37:47 +0530

    Revert "summa da deepa"
    This reverts commit 123a239b3d6a679c414ef0d58bcf18dff796300c.

--- Controller/loraController.js
+++ Controller/loraController.js
@@ -1,20 +1,20 @@
-const { LoRa, User } = require("../Models/models");
-const { ObjectId } = require("mongoose");
+const { LoRa, User  } = require("../Models/models");
+const {ObjectId} = require("mongoose")
 
-const getUserDetails = async (req, res) => {
-    try {
-        const loraid = req.body.loRaID;
-        const LoRaDetails = await LoRa.findById(loraid);
+const getUserDetails = async(req,res) => {
+try{
+        const loraid = req.body.loRaID
+        const LoRaDetails = await LoRa.findById(loraid)
+        
+        const userId = LoRaDetails.userId
 
-        const userId = LoRaDetails.userId;
+        const userDetails = await User.findById(userId)
 
-        const userDetails = await User.findById(userId);
-
-        res.status(200).json({ user: userDetails });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ response: "unable to find user" });
+        res.status(200).json({user: userDetails})
+    }catch(err){
+        console.error(err)
+        res.status(500).json({response: "unable to find user"})
     }
-};
+}
 
-module.exports = { getUserDetails };
+module.exports = {getUserDetails}
